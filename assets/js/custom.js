2a2bae9fcbac163d736c3c05ead7ed075cab5645
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2016-07-27 11:54:26 +0700

    initial commit by mczal

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+var nav = false;
+$('#container').click(function(){
+  console.log('clicked');
+  if(nav === true){
+    closeNav();
+  }
+});
+
+function openNav(){
+  document.getElementById("leftside-nav-bar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  nav = true;
+}
+
+function closeNav() {
+    document.getElementById("leftside-nav-bar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+    nav = false;
+}
