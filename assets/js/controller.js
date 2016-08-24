4acb9fb4ee07f5c8b90bb0a339329b863f07261d
Fahrizal Septrianto <fahrizalseptrianto@gmail.com>
2016-08-24 11:41:09 +0700

    active json : controller positions all done

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -7,7 +7,15 @@ beirutController.controller('CandidatePositionController',['$scope','$http','$ro
 beirutController.controller('PositionController',['$scope','$http','$routeParams', function($scope,$http,$routeParams){
 	changeTitleHeader("Jobs Management");
 	$http.get('assets/json/positions.json').success(function(response){
-		// console.log(data);
-		$scope.data = response;
+		// console.log(response);
+		$scope.positionsContent = response.content;
+		console.log($scope.positionsContent);
 	});
+
+	$scope.clickPosition = function(id){
+		console.log($scope.itr);
+		$scope.activeData = $scope.positionsContent[id];
+		console.log($scope.activeData);
+	}
+
 }]);
